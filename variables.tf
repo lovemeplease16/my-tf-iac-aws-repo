dab84d8f6f2774011c56828edfe0471850927a4d
Collins Orock <97584088+collinsako@users.noreply.github.com>
2023-02-18 01:47:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "demo-keypair"
 }
 
 variable "instance_type" {
@@ -24,6 +24,6 @@ variable "tag_name" {
 }
 
 variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  description = "AMI for Amazon linux Ec2 instance"
+  default     = "ami-0dfcb1ef8550277af"
 }
