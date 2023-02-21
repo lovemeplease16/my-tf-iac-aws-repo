64840ece5ee7feb90f0337fda3e949dc961a32f1
lovemeplease16 <123746929+lovemeplease16@users.noreply.github.com>
2023-02-21 13:29:57 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,5 +25,5 @@ variable "tag_name" {
 
 variable "ami_id" {
   description = "AMI for Amazon linux Ec2 instance"
-  default     = "ami-0cd7323ab3e63805f"
+  default     = "ami-065bb5126e4504910"
 }
